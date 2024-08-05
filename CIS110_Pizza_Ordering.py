92a4023d3b1f1aeaa69c55d8830f8fa90bf9f711
username <useremail>
2024-08-05 04:08:23 -0700

    Added Data Validation While Loops.

--- CIS110_Pizza_Ordering.py
+++ CIS110_Pizza_Ordering.py
@@ -1,6 +1,8 @@
 print("Hello, my name is Kora your vitual assistant. I will help you order a pizza!")
 print("I am going to ask you a few questions. After typing an answer, press Enter.")
 userName = input("\nEnter your name:  ")
+while len(userName) == 0:
+    userName = input("Name cannot be blank! Please enter your name: ")
 if userName.lower() == "cortez":
     print(f"\nMy creator, {userName}. what's up dude let's order some magic!")
 else:
@@ -8,11 +10,24 @@ else:
           
 
 size = input("\nWhat size do you want?  Enter small, medium, or large:  ")
+while size.lower() not in["small", "medium", "large"]:
+    size= input("Invalid value! Please enter small,medium, or large: ")
+    
 flavor = input("\nEnter the flavor of pizza:  ")
+while len(flavor) ==0:
+    flavor = input("Flavor cannot be blank! Please enter a flavor:  ")
+
 crustType = input("\nWhat type of crust do you want:  ")
+while len(crustType) ==0:
+    crustType = input("Crust type cannot be blank! Please enter crust type: ")
 quantity = input("\nHow many of these do you want to order?  Enter a numberic value:  ")
+while not quantity.isdigit():
+    quantity =input("Value not recognized. Please enter a numeric value:  ")
 quantity = int(quantity)
+
 method = input("\nIs this order carry out or delivery?:  ")
+while method not in ["carry out", "delivery"]:
+    method = input("Invalid Value!  Please enter carry out of delivery:  ")
 
 if method.lower() =="delivery":
     deliveryFee = 5
